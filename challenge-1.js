cb5f87cfdd131837b562a9d91de5f90daad40285
Amarah Abusohyon <amarhbishtawi@gmail.com>
2021-03-30 13:42:30 +0300

    done

--- challenge-1.js
+++ challenge-1.js
@@ -6,3 +6,10 @@ function square(n){
 }
 
 alert(square(num));
+let s=[];
+let array=[1,2,3,4,5];
+for(let i=0;i<array.length;i++){
+    s.push(square(array[i]));
+}
+console.log(s);
+alert('look to console');
